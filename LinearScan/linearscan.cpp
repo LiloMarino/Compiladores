31d94be4d80f68b14301d7b6985a0196d95429c6
LiloMarino <lilo.marino@hotmail.com>
2025-02-03 15:05:04 -0300

    fix: :wrench: Corrigido léxico

--- LinearScan/linearscan.cpp
+++ LinearScan/linearscan.cpp
@@ -1,1 +1,14 @@
 #include "linearscan.hpp"
+#include <iostream>
+
+void LinearScan::start(const int K)
+{
+    this->K = K;
+}
+
+void LinearScan::addVirtualRegister(int virtualRegister, std::list<int> lifeTime)
+{
+    int start = lifeTime.front();
+    int end = lifeTime.back();
+    lifeTimes[virtualRegister] = {start, end};
+}
